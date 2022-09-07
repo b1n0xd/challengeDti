d89cfb7f7728cb1579d0cc8f082cf166747a10e1
lvieira <contato@lucianovieirapro.com>
2022-09-07 14:54:40 -0300

    refactor(function): ADD task editing functionality.

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,7 +1,7 @@
-
-from django.shortcuts import render, redirect, get_object_or_404 # novo
+from django.shortcuts import render, redirect, get_object_or_404  # novo
 from .models import Tarefa
-from .forms import AdicionarTarefa
+from .forms import AdicionarTarefa, EditarTarefaForm
+
 
 def tarefas_pendentes_list(request):
     tarefas_pendentes = Tarefa.objects.filter(status='pendente')
@@ -13,14 +13,16 @@ def tarefas_pendentes_list(request):
     else:
         form = AdicionarTarefa()
     return render(request, 'tarefas/tarefas_pendentes.html',
-                  {'tarefas_pendentes':tarefas_pendentes,
-                   'form':form})
+                  {'tarefas_pendentes': tarefas_pendentes,
+                   'form': form})
+
 
 def concluir_tarefa(request, tarefa_id):
     tarefa = get_object_or_404(Tarefa, id=tarefa_id)
     tarefa.status = 'concluído'
     tarefa.save()
     return redirect('tarefas_pendentes_list')
+
 
 def excluir_tarefa(request, tarefa_id):
     tarefa = get_object_or_404(Tarefa, id=tarefa_id)
@@ -33,3 +35,19 @@ def adiar_tarefa(request, tarefa_id):
     tarefa.status = 'adiado'
     tarefa.save()
     return redirect('tarefas_pendentes_list')
+
+
+# novo
+def editar_tarefa(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, id=tarefa_id)
+    if request.method == 'POST':
+        form = EditarTarefaForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            tarefa.descricao = cd['tarefa']
+            tarefa.categoria = cd['categoria']
+            tarefa.save()
+            return redirect('tarefas_pendentes_list')
+    else:
+        form = EditarTarefaForm(initial={'tarefa': tarefa.descricao, 'categoria': tarefa.categoria})
+    return render(request, 'tarefas/editar_tarefa.html', {'tarefa': tarefa, 'form': form})
