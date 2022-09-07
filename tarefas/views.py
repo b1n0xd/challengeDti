61e82ac9638933c24e7d65f1e92e2e279715e001
lvieira <contato@lucianovieirapro.com>
2022-09-07 11:26:17 -0300

    refactor(new function): add new function "concluir tarefa" to finish a task after stay completed.

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404 # novo
+from .models import Tarefa
+from .forms import AdicionarTarefa
 
-# Create your views here.
+def tarefas_pendentes_list(request):
+    tarefas_pendentes = Tarefa.objects.filter(status='pendente')
+    if request.method == 'POST':
+        form = AdicionarTarefa(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tarefas_pendentes_list')
+    else:
+        form = AdicionarTarefa()
+    return render(request, 'tarefas/tarefas_pendentes.html',
+                  {'tarefas_pendentes':tarefas_pendentes,
+                   'form':form})
+
+# novo
+def concluir_tarefa(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, id=tarefa_id)
+    tarefa.status = 'concluído'
+    tarefa.save()
+    return redirect('tarefas_pendentes_list')
